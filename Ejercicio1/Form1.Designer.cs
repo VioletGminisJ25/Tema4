188c6accf6ed0faa6bf0aafcc2a67a299593226b
VioletGminisJ25 <javierrodal25@gmail.com>
2024-10-30 10:57:45 +0100

    adding exercise 1 and sln file

--- Ejercicio1/Form1.Designer.cs
+++ Ejercicio1/Form1.Designer.cs
@@ -42,7 +42,9 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
             this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
             // 
             // button2
             // 
@@ -53,7 +55,9 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
             this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
             // 
             // Form1
             // 
@@ -63,14 +67,17 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
-            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button1_KeyPress);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
             this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
             this.ResumeLayout(false);
 
         }
--- Ejercicio1/Form1.cs
+++ Ejercicio1/Form1.cs
@@ -9,12 +9,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-//TODO: ARREGAR FUNCTION BUTTONS Y PONER LAS DIRECTIVAS DEL COMPILADOR
+//TODO: ARREGAR FUNCTION BUTTONS Y PONER LAS DIRECTIVAS DEL COMPILADOR SOLVED
 namespace Ejercicio1
 {
     public partial class Form1 : Form
     {
         private string DEFAULT_TITLE = "Mouse Tester";
+
         public Form1()
         {
             Point initialPosition = new Point(191, 191);
@@ -27,7 +28,9 @@ namespace Ejercicio1
                 buttons[i] = new Button();
                 buttons[i].Text = i + 1 + "";
                 buttons[i].Location = currentPosition;
-                buttons[i].MouseClick += (object sender, MouseEventArgs e) =>
+                buttons[i].MouseMove += Form1_MouseMove;
+                //buttons[i].KeyDown += button1_KeyDown;
+                buttons[i].Click += (object sender, EventArgs e) =>
                 {
                     this.Text = ((Button)sender).Text;
                 };
@@ -46,7 +49,7 @@ namespace Ejercicio1
         {
             if (sender is Button)
             {
-                this.Text = $"X:{(e.X + e.Location.X)} Y:{(e.Y + e.Location.Y)}";
+                this.Text = $"X:{(e.X + ((Control)sender).Location.X)} Y:{(e.Y + ((Control)sender).Location.Y)}";
             }
             else
             {
@@ -56,12 +59,70 @@ namespace Ejercicio1
 
         }
 
-        private void Form1_MouseClick(object sender, MouseEventArgs e)
+    
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
         {
-            button1.BackColor = Color.White;
-            button2.BackColor = Color.White;
+            this.Text = DEFAULT_TITLE;
+        }
+#if KEY_EFFECT
+        private void button1_KeyDown(object sender, KeyEventArgs e) 
+        {
+            this.Text = e.KeyCode == (Keys.Escape) ? DEFAULT_TITLE : (e.KeyValue + "");
+            this.Text = e.KeyCode == (Keys.Escape) ? DEFAULT_TITLE : (e.KeyValue + "");
 
+        }
+         private void button1_KeyPress(object sender, KeyPressEventArgs e){}
+#else    
+        private void button1_KeyPress(object sender, KeyPressEventArgs e)
+        {
 
+            this.Text = e.KeyChar== ((char)Keys.Escape) ? DEFAULT_TITLE : (e.KeyChar + "");
+            this.Text = e.KeyChar== ((char)Keys.Escape) ? DEFAULT_TITLE : (e.KeyChar+ "");
+
+        }
+        private void button1_KeyDown(object sender, KeyEventArgs e) { }
+#endif
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("¿Seguro que desea salir?", "Mi Aplicación", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                button1.BackColor = Color.White;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                button2.BackColor = Color.White;
+            }
+            else
+            {
+                button1.BackColor = Color.White;
+                button2.BackColor = Color.White;
+
+            }
+
+          
+        }
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
             if (e.Button == MouseButtons.Left)
             {
                 button1.BackColor = Color.Blue;
@@ -75,34 +136,6 @@ namespace Ejercicio1
                 button1.BackColor = Color.Blue;
                 button2.BackColor = Color.Red;
 
-            }
-        }
-
-        private void Form1_MouseLeave(object sender, EventArgs e)
-        {
-            this.Text = DEFAULT_TITLE;
-        }
-
-        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-
-        }
-
-        private void button1_KeyDown(object sender, KeyEventArgs e) 
-        {
-            this.Text = e.KeyCode == (Keys.Escape) ? DEFAULT_TITLE : (e.KeyValue + "");
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            if (MessageBox.Show("¿Seguro que desea salir?", "Mi Aplicación", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Cancel)
-            {
-                e.Cancel = true;
             }
         }
     }
