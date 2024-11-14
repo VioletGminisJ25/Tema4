5468ad3f9ef742da5ef4a65d60daef22d6acd34e
VioletGminisJ25 <javierrodal25@gmail.com>
2024-11-14 18:53:13 +0100

    ejercicio 3 hecho

---
+++ Ejercicio 4/Form2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_4
+{
+    public partial class Form2 : Form
+    {
+        public Form2(String filePath)
+        {
+            InitializeComponent();
+            pictureBox1.Image=new Bitmap($@"{filePath}");
+            String[] path = filePath.Split('\\');
+            this.Text = path[path.Length-1];
+        }
+    }
+}
