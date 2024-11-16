24db8a36050fbda1e1d17365b7365e7a99ebcad7
VioletGminisJ25 <javierrodal25@gmail.com>
2024-11-16 17:36:54 +0100

    ejercicio 5 casi completo

--- ejercicio5/Form1.Designer.cs
+++ ejercicio5/Form1.Designer.cs
@@ -28,13 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listRight = new System.Windows.Forms.ListBox();
+            this.listLeft = new System.Windows.Forms.ListBox();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.lblItems = new System.Windows.Forms.Label();
+            this.lblIndex = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listRight
+            // 
+            this.listRight.FormattingEnabled = true;
+            this.listRight.Location = new System.Drawing.Point(463, 92);
+            this.listRight.Name = "listRight";
+            this.listRight.Size = new System.Drawing.Size(169, 186);
+            this.listRight.TabIndex = 0;
+            // 
+            // listLeft
+            // 
+            this.listLeft.FormattingEnabled = true;
+            this.listLeft.Location = new System.Drawing.Point(152, 91);
+            this.listLeft.Name = "listLeft";
+            this.listLeft.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listLeft.Size = new System.Drawing.Size(157, 186);
+            this.listLeft.TabIndex = 1;
+            this.listLeft.SelectedIndexChanged += new System.EventHandler(this.listLeft_SelectedIndexChanged);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(345, 147);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 23);
+            this.btnRight.TabIndex = 2;
+            this.btnRight.Text = "Move > >";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(345, 190);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnLeft.TabIndex = 3;
+            this.btnLeft.Text = "< <  Move";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(440, 290);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(224, 293);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(320, 293);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(100, 20);
+            this.textBox.TabIndex = 6;
+            // 
+            // lblItems
+            // 
+            this.lblItems.AutoSize = true;
+            this.lblItems.Location = new System.Drawing.Point(240, 65);
+            this.lblItems.Name = "lblItems";
+            this.lblItems.Size = new System.Drawing.Size(0, 13);
+            this.lblItems.TabIndex = 7;
+            // 
+            // lblIndex
+            // 
+            this.lblIndex.AutoSize = true;
+            this.lblIndex.Location = new System.Drawing.Point(100, 92);
+            this.lblIndex.Name = "lblIndex";
+            this.lblIndex.Size = new System.Drawing.Size(0, 13);
+            this.lblIndex.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblIndex);
+            this.Controls.Add(this.lblItems);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.listLeft);
+            this.Controls.Add(this.listRight);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listRight;
+        private System.Windows.Forms.ListBox listLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label lblItems;
+        private System.Windows.Forms.Label lblIndex;
     }
 }
 
--- ejercicio5/Form1.cs
+++ ejercicio5/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,82 @@ namespace ejercicio5
         public Form1()
         {
             InitializeComponent();
+            actualizarLabel();
+            lblIndex.Text = "Index: \n";
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(textBox.Text.Trim() != "")
+            {
+                if (!listLeft.Items.Contains(textBox.Text)) {
+                    listLeft.Items.Add(textBox.Text);
+                }
+            }
+            actualizarLabel();
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (listLeft.SelectedItems != null) {
+                for(int i = listLeft.SelectedItems.Count - 1; i >= 0; i--)
+                {
+                    listLeft.Items.RemoveAt(i);
+                }
+            }
+            actualizarLabel();
+
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            ArrayList elements = elements = new ArrayList();
+
+            foreach (var item in listLeft.SelectedItems)
+            {
+                elements.Add(item);
+            }
+
+            foreach (var item in elements)
+            {
+                listRight.Items.Add(item);
+                listLeft.Items.Remove(item);
+            }
+            actualizarLabel();
+
+        }
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            ArrayList elements = elements = new ArrayList();
+
+            foreach (var item in listRight.SelectedItems)
+            {
+                elements.Add(item);
+            }
+
+            foreach (var item in elements)
+            {
+                listLeft.Items.Add(item);
+                listRight.Items.Remove(item);
+            }
+            actualizarLabel();
+
+        }
+
+
+        public void actualizarLabel()
+        {
+            lblItems.Text = "List Count: " + listLeft.Items.Count.ToString();
+        }
+
+        private void listLeft_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblIndex.Text = "Index: \n";
+            foreach (var item in listLeft.SelectedItems)
+            {
+                lblIndex.Text += listLeft.Items.IndexOf(item) +"\n";
+            }
         }
     }
 }
