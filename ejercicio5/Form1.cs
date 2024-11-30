ebc2186519427647ca4ccb735f0cccc9792e3bf6
VioletGminisJ25 <javierrodal25@gmail.com>
2024-11-30 17:16:50 +0100

    ejercicio5 COMPLETO

--- ejercicio5/Form1.cs
+++ ejercicio5/Form1.cs
@@ -13,18 +13,42 @@ namespace ejercicio5
 {
     public partial class Form1 : Form
     {
+        Timer timer;
+        int cont;
+        private String title;
         public Form1()
         {
             InitializeComponent();
             actualizarLabel();
             lblIndex.Text = "Index: \n";
+            title = "Ejercicio5";
+            cont = title.Length - 1;
+            timer = new Timer();
+            timer.Tick += AnimacionTitulo;
+            timer.Interval = 200;
+            timer.Start();
+        }
+
+        private void AnimacionTitulo(object sender, EventArgs e)
+        {
+            if (cont >= 0)
+            {
+
+                this.Text = title.Substring(cont);
+                cont--;
+            }
+            else
+            {
+                cont = title.Length - 1;
+            }
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            if(textBox.Text.Trim() != "")
+            if (textBox.Text.Trim() != "")
             {
-                if (!listLeft.Items.Contains(textBox.Text)) {
+                if (!listLeft.Items.Contains(textBox.Text))
+                {
                     listLeft.Items.Add(textBox.Text);
                 }
             }
@@ -34,10 +58,11 @@ namespace ejercicio5
 
         private void btnRemove_Click(object sender, EventArgs e)
         {
-            if (listLeft.SelectedItems != null) {
-                for(int i = listLeft.SelectedItems.Count - 1; i >= 0; i--)
+            if (listLeft.SelectedItems != null)
+            {
+                for (int i = listLeft.Items.Count - 1; i >= 0; i--)
                 {
-                    listLeft.Items.RemoveAt(i);
+                    listLeft.Items.Remove(listLeft.SelectedItem);
                 }
             }
             actualizarLabel();
@@ -90,7 +115,7 @@ namespace ejercicio5
             lblIndex.Text = "Index: \n";
             foreach (var item in listLeft.SelectedItems)
             {
-                lblIndex.Text += listLeft.Items.IndexOf(item) +"\n";
+                lblIndex.Text += listLeft.Items.IndexOf(item) + "\n";
             }
         }
     }
